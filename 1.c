82ff0dffd9258fa1f907631284d77fd325e5f1f5
Svetlana025 <44404399+Svetlana025@users.noreply.github.com>
2019-01-10 00:11:31 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main()
+{
+float a, x, G, Y, F;
 	
-	float a, x, G, Y, F;
+printf ("Введите число a \n");
+scanf ("%f", &a);
+printf ("Введите число x \n");
+scanf ("%f", &x);
+
+G = - (3* (14*(a*2) + 23*a*x - 30*(x*2)))/(-9*(a*2)) + 37*a*x + 40*(x*2));
+F = - tan(18*(a*2) - a*x - 4*(x*2));
+Y = (log(35*(a*2) - 27*a*x + 4*(x*2) +1))/(log(2));
+
+printf("G=%f\n",G);
+printf("F=%f\n",F);
+printf("Y=%f\n",Y);
 	
-	// result of G
-	printf("Enter a and x\n");
-	scanf("%f %f",&a,&x);
-	G = 0 - (3* (14*pow(a,2) + 23*a*x - 30*pow(x,2)))/( (0-(9*pow(a,2))) + 37*a*x + 40*pow(x,2)); 
-	printf("G = %f\n",G);
-	
-	// result of F
-	printf("Enter a and x\n");
-	scanf("%f %f",&a,&x);
-	F = 0 - tan(18*pow(a,2) - a*x - 4*pow(x,2));
-	printf("\nF = %f\n",F); 
-	
-	// result of Y
-	printf("Enter a and x\n");
-	scanf("%f %f",&a,&x);
-	Y = (log(35*pow(a,2) - 27*a*x + 4*pow(x,2) +1))/log(2);
-	printf("\nY = %f\n",Y);
-	
-	return 0;
+return 0;
 }
